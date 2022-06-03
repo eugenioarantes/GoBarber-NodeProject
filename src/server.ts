1e2d59089a059dd9fb56017558c29e0e0f74bb11
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-06-03 13:05:04 -0300

    creating and configuring database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express, { request } from 'express';
+import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -7,5 +9,6 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
 });
