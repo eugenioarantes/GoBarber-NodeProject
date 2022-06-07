0538b67e65f1bb2cb8d670d71790e816c602669a
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-06-07 14:07:21 -0300

    creating error class

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,9 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 
 import { sign } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 import authConfig from '../config/auth';
@@ -23,13 +26,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect username/password combination.');
+      throw new AppError('Incorrect username/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect username/password combination.');
+      throw new AppError('Incorrect username/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
