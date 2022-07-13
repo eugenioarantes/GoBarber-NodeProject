f24a709c767c0d4eacdda6b5c81300aeaf6b1a5c
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-07-13 10:34:26 -0300

    refactoring tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -3,17 +3,20 @@ import AppError from '@shared/errors/AppError';
 import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointService from './CreateAppointmentService';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let fakeNotificationsRepository: FakeNotificationsRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let createAppointment: CreateAppointService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
    fakeAppointmentsRepository = new FakeAppointmentsRepository();
    fakeNotificationsRepository = new FakeNotificationsRepository();
+   fakeCacheProvider = new FakeCacheProvider();
 
-   createAppointment = new CreateAppointService(fakeAppointmentsRepository, fakeNotificationsRepository);
+   createAppointment = new CreateAppointService(fakeAppointmentsRepository, fakeNotificationsRepository, fakeCacheProvider);
   })
 
   it('should be able to create a new Appointment', async () => {
