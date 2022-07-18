9b667aad96941a4f445e7da5e20886d6a2d84d84
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-07-18 09:35:43 -0300

    adding cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
@@ -16,6 +17,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
